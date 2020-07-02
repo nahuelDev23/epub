9b35dd983be8908d4932bab5001be0787ff873f9
Nahuel Jotandjota <nahuel.dev.23@gmail.com>
2020-07-02 10:10:37 -0300

    Procfile

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('mysql://b027148960a2c6:da588616@us-cdbr-east-02.cleardb.com/heroku_b429cd0d4fbdf1a?reconnect=true'),
+            'host' => env('DB_HOST', 'us-cdbr-east-02.cleardb.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_b429cd0d4fbdf1a'),
+            'username' => env('DB_USERNAME', 'b027148960a2c6'),
+            'password' => env('DB_PASSWORD', 'da588616'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
