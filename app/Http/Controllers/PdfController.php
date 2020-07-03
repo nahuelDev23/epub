3b0fa82e44d4fbe2fa24fd85f6a3886cf5428cf3
Nahuel Jotandjota <nahuel.dev.23@gmail.com>
2020-07-03 11:30:08 -0300

    sto

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -42,7 +42,7 @@ class PdfController extends Controller
         if ($request->hasFile('epub')) {
             //$pdf->pdf = $request->file('epub')->store('uploads', 'public');
             $pdf->pdf = $request->file('epub');
-            $path = Storage::disk('uploads');
+            $path = Storage::disk();
         }
         $pdf->save();
         return redirect()->route('pdf.index',$pdf->id)->with('info','Libro guardado con éxito');
