3cf05f40dcc71d85dce240cf6a425bc89e516a80
Nahuel Jotandjota <nahuel.dev.23@gmail.com>
2020-07-03 22:54:39 -0300

    ssl .htaccess

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -3,18 +3,5 @@
 @section('content')
 
     <h1>You are currently not connected to any networks.</h1>
-    @foreach($pdf as $libro)
-                  <tr>
-                        <td>{{$libro->name}}</td>
-                        <td><a href="{{route('pdf.show',$libro->id)}}">Ver</a></td>
-                        <td>
-                              {!!Form::open(['route' => ['pdf.destroy',$libro->id],
-                                    'method' => 'DELETE']) !!}
-                                    <button class="btn btn-sm btn-danger">Eliminar</button>
-                              {!!Form::close()!!}
-                        </td>
-                        
-                  </tr>
-                  @endforeach
     <h2>debajo</h2>
 @endsection
