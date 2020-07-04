a4c8826104a63e1246e50597b67d9ac02ddd8131
Nahuel Jotandjota <nahuel.dev.23@gmail.com>
2020-07-03 21:57:49 -0300

    ssl .htaccess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (app()->environment('remote')) {
+        if (app()->environment('production')) {
             URL::forceSchema('https');
         }
     }
